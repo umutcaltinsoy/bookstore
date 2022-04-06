40bc2c111e251a58d0ec10ae84dda808dadcba04
umutaltinsoy <umutcaltinsoy@gmail.com>
2022-04-06 04:30:26 +0300

    Validations

--- src/main/java/com/altinsoy/bookstore/dto/BookDto.java
+++ src/main/java/com/altinsoy/bookstore/dto/BookDto.java
@@ -5,14 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BookDto {
+    @NotEmpty
+    @Size(min = 3, message = "Name should be at least 3 characters")
     private String bookName;
+    @NotEmpty
+    @Size(min = 3, message = "Author is mandatory")
     private String author;
+    @NotEmpty
     private Double price;
+    @NotEmpty
     private Integer unitsInStock;
 
 }
--- src/main/java/com/altinsoy/bookstore/dto/CustomerDto.java
+++ src/main/java/com/altinsoy/bookstore/dto/CustomerDto.java
@@ -5,13 +5,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class CustomerDto {
+    @NotEmpty
+    @Size(min = 11, max = 11, message = "Identity number should be at least 11 digits")
     private String identityNumber;
+
+    @NotEmpty
+    @Size(min = 3, message = "Name should be at least 3 characters")
     private String customerName;
+
+    @NotEmpty
+    @Size(min = 2, message = "Surname should be at least 2 characters")
     private String customerSurname;
+
+    @Email
     private String email;
 }
